58bcabd7b5c60a5294d9f274e0f426a39f2606cf
Anders Goncalves da Silva <andersgs@gmail.com>
2013-10-03 15:15:14 +1000

    Fixed parse_fastq to work be able to load into interpreter

--- parse_fastq.py
+++ parse_fastq.py
@@ -3,10 +3,7 @@ from Bio import SeqIO
 from sickle_sequence_class import OurSequence
 from test_func import is_zero_len, is_allN, length_match
 
-fastq_file = sys.argv[1]
-fastq_dict = {}
-
-def makeFastqDict(sequence_object):
+def makeFastqDict(sequence_object,fastq_dict):
 
 	'''Make a dictionary where the key is the id of the read
 	and the value is the OurSequence object'''
@@ -22,7 +19,7 @@ def getSequenceObject(fastq_file):
 
 	'''Take each individual record in the fastq file and convert these into
 	their own OurSequence objects.'''
-	
+	fastq_dict = {}
 	for record in SeqIO.parse(fastq_file, "fastq"):
 		
 		#get identifying information
@@ -48,7 +45,15 @@ def getSequenceObject(fastq_file):
 #		print new_sequence_object.sequence
 
 		#add new sequence object to dictionary
-		makeFastqDict(new_sequence_object)
+		makeFastqDict(new_sequence_object,fastq_dict)
+	return fastq_dict
 
-getSequenceObject(fastq_file)
-print fastq_dict
+
+
+if __name__=='__main__':
+	
+	fastq_file = sys.argv[1]
+	fastq_dict = {}
+	getSequenceObject(fastq_file)
+	print fastq_dict
+
