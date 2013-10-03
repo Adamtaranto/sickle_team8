a80807e17c79dd34ba671a576b90b3fd4ed2d113
jhawkey <jhawkey@student.unimelb.edu.au>
2013-10-03 14:02:56 +1000

    added mean and sd functions

--- sickle_sequence_class.py
+++ sickle_sequence_class.py
@@ -1,3 +1,5 @@
+import math
+
 class OurSequence:
 	'''
 	This class will keep track of a read quality scores.
@@ -7,12 +9,42 @@ class OurSequence:
 		self.readid = readid
 		self.sequence = sequence
 		self.quality = quality
-	
+		self.meanq = self.mean()
+
 	def mean(self):
-		pass
+	
+		'''Calculate the mean quality score for an entire read.'''
+		
+		#get total number of quality scores and
+		#sum quality scores together
+		total_len = len(self.quality)
+		total_sum = sum(self.quality)
+		
+		#calculate mean
+		mean_read = float(total_sum) / total_sum
+		return mean_read
 	
 	def sd(self):
-		pass
+	
+		'''Calculate the standard deviation of the quality socres
+		for an entire read.'''
+		
+		#list to store variance values in
+		new_variance = []
+		
+		#loop over quality scores and calculate variance
+		for i in quality:
+			variance = pow((i - self.meanq), 2)
+			new_variance.append(variance)
+		
+		#sum the variance values together
+		variance_sum = sum(new_variance)
+		#get the number of variance values
+		variance_total = len(new_variance)
+		
+		#caluculate the standard deviation
+		standard_dev = float(variance_sum) / variance_total
+		return standard_dev
 	
 	def window_mean(self,window_size, offset):
 		pass
